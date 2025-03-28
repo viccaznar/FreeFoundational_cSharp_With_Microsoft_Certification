8d5cf2de1d92d48abcd3f17e1eed265bd96211e1
viccaznar <viccaznar@yahoo.com>
2025-03-28 07:45:02 -0300

    Unidade de iteração

---
+++ projects/iterarIndiceTrasPraFrente/iterandoIndiceTrasPraFrente.cs
@@ -0,0 +1,5 @@
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
---
+++ projects/permissionLevelAdmin/permissionLevelAdmin.cs
@@ -0,0 +1,29 @@
+﻿string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have the required permission.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficent privileges.");
+}
